5fec746d9e926a805905ae01f05e492ce6fbf354
Parker Joncus <parkerj@me.com>
2020-09-17 14:19:23 -0500

    add join button funtionality

--- adnatBackend/challenge/app/controllers/organizations_controller.rb
+++ adnatBackend/challenge/app/controllers/organizations_controller.rb
@@ -10,6 +10,12 @@ class OrganizationsController < ApplicationController
         @user = User.find(params[:id])
     end
     
+    def show
+        @user = User.find(params[:id])
+        @user.organization_ids = params[:user_id]
+        redirect_to user_path(@user)
+    end
+    
     def update
         @organization = Organization.find(params[:user_id])
         @user = User.find(params[:id])
