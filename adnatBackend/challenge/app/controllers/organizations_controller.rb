b78d90f75b755c44babb6f1842a3d13ddeff334a
Parker Joncus <parkerj@me.com>
2020-09-17 11:17:04 -0500

    allow adding, editing and deleting organizations

--- adnatBackend/challenge/app/controllers/organizations_controller.rb
+++ adnatBackend/challenge/app/controllers/organizations_controller.rb
@@ -5,6 +5,30 @@ class OrganizationsController < ApplicationController
         redirect_to user_path(@user)
     end
     
+    def edit
+        @organization = Organization.find(params[:user_id])
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @organization = Organization.find(params[:user_id])
+        @user = User.find(params[:id])
+        
+        if @organization.update(organization_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @organization = Organization.find(params[:user_id])
+        @user = User.find(params[:id])
+        
+        @organization.destroy
+        redirect_to @user
+    end
+    
     private
         def organization_params
             params.require(:organization).permit(:name, :hourlyrate)
